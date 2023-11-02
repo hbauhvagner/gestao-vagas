1ce4643848cda8c1f08b2e22cf2aefaca0153bb6
hbauhvagner <hbauhvagner@gmail.com>
2023-11-02 01:49:16 -0300

    Formatando mensagem de validação

---
+++ src/main/java/br/com/hbauhvagner/gestao_vagas/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,12 @@
+package br.com.hbauhvagner.gestao_vagas.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+    
+    private String message;
+    private String field;
+}
